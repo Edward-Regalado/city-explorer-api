3c1e431dc3b9cc135ce732021a39c5ca2672a656
Edward Regalado <tregalado30@gmail.com>
2021-04-26 17:02:08 -0700

    finished lab 08

--- server.js
+++ server.js
@@ -4,9 +4,12 @@
 const express = require('express');
 require('dotenv').config();
 const cors = require('cors');
+const superagent = require('superagent');
 
 // importing the objects from the weather.json file and assign to varible so we can use it.
 const weatherData = require('./data/weather.json');
+const { request } = require('express');
+
 
 const app = express();
 app.use(cors());
@@ -14,23 +17,62 @@ app.use(cors());
 // PORT set to process.env.PORT in .env or PORT 3001 for default
 const PORT = process.env.PORT || 3001;
 
-app.get('/weather', (request, response) => {
-  try {
-    const mappedWeatherData = weatherData.data.map(day => new WeatherForecast(day));
-    response.send(mappedWeatherData);
-  } catch (error) {
-    handleErrors(error, response);
-  }
-})
-
 function WeatherForecast(day) {
   this.date = day.datetime;
   this.description = day.weather.description;
 }
 
-function handleErrors(err, response) {
-  response.status(500).send('Internal error');
+function Movies(infor) {
+  this.title = infor.title;
+  this.overview = infor.overview;
 }
+
+// function handleErrors(err, response) {
+// response.status(500).send('Internal error');
+// }
+
+
+// app.get('/weather', (request, response) => {
+// try {
+// const mappedWeatherData = weatherData.data.map(day => new WeatherForecast(day));
+// response.send(mappedWeatherData);
+// } catch (error) {
+// handleErrors(error, response);
+// }
+// });
+
+app.get('/', (request, response) => {
+  response.send('hello');
+});
+
+app.get('/weather', (request, response) => {
+  superagent.get('https://api.weatherbit.io/v2.0/forecast/daily')
+    .query({
+      key: process.env.WEATHERBIT_API_KEY,
+      unit: 'I',
+      lat: request.query.lat,
+      lon: request.query.lon
+    })
+    .then(weatherData => {
+      // console.log(weatherData.body.city_name);
+      response.send(weatherData.body.data.map(day => (new WeatherForecast(day))));
+    })
+    .catch(err => (err.request, err.response));
+});
+
+
+// https://api.themoviedb.org/3/search/movie?api_key=bc8774b1b4d020c62bb62615cad7746c&query=seattle
+app.get('/movies', (request, response) => {
+  superagent.get('https://api.themoviedb.org/3/search/movie')
+    .query({
+      api_key: process.env.MOVIE_API_KEY,
+      query: request.query.city,
+    })
+    .then(movieInfor => {
+      response.send(movieInfor.body.results.map(infor => (new Movies(infor))));
+    })
+    .catch(err => (err.request, err.response));
+});
 
 app.listen(PORT, () => console.log(`Listening on ${PORT}`));
 
